e734b420835a9198e0eb9e0146b10e3ea9e81719
alexseuf <48165001+alexseuf@users.noreply.github.com>
2026-07-12 20:32:27 +0200

    feat: add UI performance metrics

--- include/ui.h
+++ include/ui.h
@@ -6,4 +6,5 @@ void begin(PowerSupplyState &state);
 void update(const PowerSupplyState &state);
 void updateTouchDiag(const TouchSample &sample);
 void handleTouch(const TouchSample &sample);
+void setPerformance(uint8_t cpuLoadPercent, uint32_t loopTimeUs);
 }
